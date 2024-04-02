2fb51ddb09c0748add57e524fe67dbcd702a5725
IjonK007 <kushta.joni@gmail.com>
2024-04-02 20:13:14 +0200

    created workout schema

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Workout = require('../models/workoutModel');
 
 const router = express.Router();
 
@@ -13,8 +14,15 @@ router.get('/:id', (req, res) => {
 })
 
 //POST a new workout
-router.post('/', (req, res) => {
-    res.json({msg: 'POST a new workout'})
+router.post('/', async (req, res) => {
+    const {title, reps, load} = req.body
+
+    try {
+        const workout = await Workout.create({title, reps, load})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 })
 
 //DELETE a workout
