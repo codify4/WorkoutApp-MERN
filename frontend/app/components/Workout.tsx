68c14dcc8be99310416a383e0bc32293a963775a
IjonK007 <kushta.joni@gmail.com>
2024-04-04 20:49:45 +0200

    font changes

--- frontend/app/components/Workout.tsx
+++ frontend/app/components/Workout.tsx
@@ -10,9 +10,15 @@ const Workout = ({ workout }: WorkoutProps) => {
 
     return (
         <div className="flex flex-col relative w-[400px] bg-[#fff] rounded-xl mt-[20px] mx-auto p-[20px] shadow-md">
-            <h1 className="mx-0 mb-[10px] mt-0 text-[1.2em] text-primary">{workout.title}</h1>
-            <p className="workout-p">Reps {workout.reps}</p>
-            <p className="workout-p">Load(kg): {workout.load}</p>
+            <h1 className="mx-0 mb-[10px] mt-0 text-[1.2em] text-primary font-extrabold">{workout.title}</h1>
+            <p className="workout-p">
+                <strong>Reps: </strong> 
+                {workout.reps}
+            </p>
+            <p className="workout-p">
+                <strong>Load(kg): </strong>
+                {workout.load}
+            </p>
             <p className="workout-p">{workout.createdAt}</p>
         </div>
     )
