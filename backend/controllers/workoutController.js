f81590068b9c3ae09be09d67e89b041b797391d9
IjonK007 <kushta.joni@gmail.com>
2024-04-06 14:10:15 +0200

    added custom error messages

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -29,6 +29,21 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
     const {title, reps, load} = req.body
 
+    let emptyFields = []
+
+    if(!title) {
+        emptyFields.push('title')
+    }
+    if(!reps) {
+        emptyFields.push('reps')
+    }
+    if(!load) {
+        emptyFields.push('load')
+    }
+    if(emptyFields.length > 0) {
+        return res.status(400).json({error: 'Please fill in all fields', ...emptyFields})
+    }
+
     // add doc to db
     try {
         const workout = await Workout.create({title, reps, load})
